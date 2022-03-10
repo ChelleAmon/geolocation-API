f900d33d58d1a3f003565b609a80f177a41958af
Gichelle Amon <amongichelle@gmail.com>
2022-03-10 11:40:54 -0800

    feat(geolocation): add function to get the current position

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,5 +1,11 @@
 if('geolocation' in navigator) {
    console.log("Geolocation is available")
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        console.log('Latitude: ', position.coords.latitude), 
+        console.log('Longitude: ', position.coords.longitude);
+    });
+  
   } else {
     console.log("Geolocation is NOT available. I'm sorry!")
   }
